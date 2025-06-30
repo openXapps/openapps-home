7e06e6829586a8d20de1672066ed136f69e2a7f5
OpenApps <32998735+openXapps@users.noreply.github.com>
2025-06-30 11:21:33 +0200

    First commit

---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { Moon, Sun } from "lucide-react"
+import { useTheme } from "next-themes"
+
+import { Button } from "@/components/ui/button"
+
+export default function ThemeToggle() {
+  const { setTheme, theme, resolvedTheme } = useTheme()
+
+  function toggleTheme() {
+    if (theme === 'system') {
+      if (resolvedTheme === 'light') setTheme('dark');
+      if (resolvedTheme === 'dark') setTheme('light');
+    }
+    if (theme === 'dark') setTheme('light');
+    if (theme === 'light') setTheme('dark');
+  }
+
+  return (
+    <Button variant="outline" size="icon" onClick={toggleTheme}>
+      <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  )
+}
